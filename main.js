af9af87a96e1d770487fbf00e0aac3980b515d0d
Student <lestoffler@gmail.com>
2015-06-22 15:10:36 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var word = prompt("Enter any word.");
+alert(word + word.length + word.charAt(2) + word.toLowerCase() + word.toUpperCase() + "You chose " + word + " , why?" + word.substring(1, 3));
+
+
+
+
+
+
+
